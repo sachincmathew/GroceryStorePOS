3fcd7a2659724d2df1b8a401dff5992e36767283
sachincmathew <sachincmathew@gmail.com>
2019-12-09 23:40:51 +0800

    TODO

--- src/main/java/com/store/Application.java
+++ src/main/java/com/store/Application.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		// TODO: JWT authentication
+		// TODO: Request-Response logging
+		// TODO: Unit Testing
+		// TODO: Fix container network issue
 		SpringApplication.run(Application.class, args);
 	}
 
