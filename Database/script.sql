edf21a57c4dfdd67674e253443cc50c997016c99
sachincmathew <sachincmathew@gmail.com>
2019-12-09 19:45:14 +0800

    updated DB scripts

--- Database/script.sql
+++ Database/script.sql
@@ -1,5 +1,44 @@
-CREATE DATABASE `db_example` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+ -- CREATE DATABASE `db_example` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
+DROP TABLE IF EXISTS `inventory`;
+CREATE TABLE `inventory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `purchase_price` decimal(13,2) NOT NULL DEFAULT '0.00',
+  `retail_price` decimal(13,2) NOT NULL DEFAULT '0.00',
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `shopping_cart`;
+CREATE TABLE `shopping_cart` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(45) NOT NULL,
+  `open_date` datetime DEFAULT NULL,
+  `close_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id_UNIQUE` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `shopping_cart_items`;
+CREATE TABLE `shopping_cart_items` (
+  `cart_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_inventory_idx` (`item_id`),
+  KEY `fk_shopping_cart` (`cart_id`),
+  CONSTRAINT `fk_inventory` FOREIGN KEY (`item_id`) REFERENCES `inventory` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_shopping_cart` FOREIGN KEY (`cart_id`) REFERENCES `shopping_cart` (`Id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `checkout_items_history`;
 CREATE TABLE `checkout_items_history` (
   `id` int(11) NOT NULL,
   `cart_id` int(11) NOT NULL,
@@ -12,36 +51,4 @@ CREATE TABLE `checkout_items_history` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `fk_shoppingcart_idx` (`cart_id`),
   CONSTRAINT `fk_shoppingcart` FOREIGN KEY (`cart_id`) REFERENCES `shopping_cart` (`Id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `inventory` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `purchase_price` decimal(13,2) NOT NULL DEFAULT '0.00',
-  `retail_price` decimal(13,2) NOT NULL DEFAULT '0.00',
-  `name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `shopping_cart` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `status` varchar(45) NOT NULL,
-  `open_date` datetime DEFAULT NULL,
-  `close_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `Id_UNIQUE` (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `shopping_cart_items` (
-  `cart_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `fk_inventory_idx` (`item_id`),
-  KEY `fk_shopping_cart` (`cart_id`),
-  CONSTRAINT `fk_inventory` FOREIGN KEY (`item_id`) REFERENCES `inventory` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_shopping_cart` FOREIGN KEY (`cart_id`) REFERENCES `shopping_cart` (`Id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
