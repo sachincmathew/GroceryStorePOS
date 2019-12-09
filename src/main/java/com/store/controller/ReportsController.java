7422b9cfb83c22c979d89d226715cb1a1089e044
sachincmathew <sachincmathew@gmail.com>
2019-12-09 22:36:01 +0800

    ReportService

--- src/main/java/com/store/controller/ReportsController.java
+++ src/main/java/com/store/controller/ReportsController.java
@@ -1,20 +1,11 @@
 package com.store.controller;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.store.model.CheckoutItemsHistory;
-import com.store.model.ShoppingCart;
-import com.store.repo.CheckoutItemsHistoryRepository;
-import com.store.repo.ShoppingCartRepository;
-
+import com.store.service.ReportService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -22,33 +13,16 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("/api/reports")
 public class ReportsController {
-	private static final String CLOSED = "CLOSED";
 
 	@Autowired
-	private ShoppingCartRepository shoppingCartRepository;
+	private ReportService reportService;
 
-	@Autowired
-	private CheckoutItemsHistoryRepository checkoutItemsRepository;
-	
 	@ApiOperation(value = "Tally's all sold items in **closed** shopping carts, and computes the total profit or loss for last specified number of days")
 	@RequestMapping(path = "/calculateProfit/{days}", method = RequestMethod.GET)
-	public Float calculateProfit(@PathVariable int days) {
-		LocalDate localDate = LocalDate.now().minusDays(days);
-		Date fromDate = java.sql.Date.valueOf(localDate);
-
-		List<ShoppingCart> lsc = shoppingCartRepository.findByStatusAndCloseDateAfter(CLOSED, fromDate);
-
-		List<Integer> cartIds = new ArrayList<Integer>();
-		for (ShoppingCart sc : lsc) {
-			cartIds.add(sc.getId());
+	public String calculateProfit(@PathVariable int days) {
+		if(days <= 0) {
+			return "invalid day count entered";
 		}
-
-		List<CheckoutItemsHistory> cih = checkoutItemsRepository.findByCartIdIn(cartIds);
-		
-		float profit = 0.0F;
-		for(CheckoutItemsHistory c: cih) {
-			profit += (c.getRetailPrice() - c.getPurchasePrice()) * c.getQuantity();
-		}
-		return profit;
+		return reportService.calculateProfit(days).toString();
 	}
 }
--- src/main/java/com/store/service/ReportService.java
+++ src/main/java/com/store/service/ReportService.java
@@ -1,5 +1,45 @@
 package com.store.service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.store.model.CheckoutItemsHistory;
+import com.store.model.ShoppingCart;
+import com.store.repo.CheckoutItemsHistoryRepository;
+import com.store.repo.ShoppingCartRepository;
+
+@Service
 public class ReportService {
+
+	private static final String CLOSED = "CLOSED";
+
+	@Autowired
+	private ShoppingCartRepository shoppingCartRepository;
+
+	@Autowired
+	private CheckoutItemsHistoryRepository checkoutItemsRepository;
+
+	public Float calculateProfit(int days) {
+		LocalDate localDate = LocalDate.now().minusDays(days);
+		Date fromDate = java.sql.Date.valueOf(localDate);
+
+		List<ShoppingCart> lsc = shoppingCartRepository.findByStatusAndCloseDateAfter(CLOSED, fromDate);
+
+		List<Integer> cartIds = new ArrayList<Integer>();
+		for (ShoppingCart sc : lsc) {
+			cartIds.add(sc.getId());
+		}
+
+		List<CheckoutItemsHistory> cih = checkoutItemsRepository.findByCartIdIn(cartIds);
+
+		float profit = 0.0F;
+		for (CheckoutItemsHistory c : cih) {
+			profit += (c.getRetailPrice() - c.getPurchasePrice()) * c.getQuantity();
+		}
+		return profit;
+	}
 
 }
