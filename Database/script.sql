75e7c88788c1915dbbc9f17ea3f51b4ee17c0aab
sachincmathew <sachincmathew@gmail.com>
2019-12-10 10:48:25 +0800

    Docker related updates

--- Database/script.sql
+++ Database/script.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `grocery_store_pos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+ -- CREATE DATABASE `grocery_store_pos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE grocery_store_pos;
 
--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,9 @@ RUN apk update \
 
 RUN git clone https://github.com/sachincmathew/GroceryStorePOS.git \
 	&& cd GroceryStorePOS \
+	&& git fetch \
+	&& git checkout fordocker \
+	&& git status \
 	&& ./gradlew build \
 	&& cp build/libs/grocery-store-pos-1.0.0.jar .. \
 	&& rm -rf /GroceryStorePOS
